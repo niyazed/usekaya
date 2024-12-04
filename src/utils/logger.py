d24a5053d6bfda3a9bb2d263184050119c8848e4
Niyaz Bin Hashem <niyaz.hashem@gmail.com>
2024-12-05 03:12:46 +0800

    fix: logs folder creation

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,10 +1,14 @@
 from loguru import logger
+import os
+
+# Create logs directory if it doesn't exist
+os.makedirs("logs", exist_ok=True)
 
 logger.add("logs/app.logs", 
            colorize=True, 
            format="<green>{time}</green> <level>{message}</level>", 
            level="INFO",
-           rotation="1 week",
+           rotation="1 week", 
            retention="1 month",
            compression="zip"
            )
