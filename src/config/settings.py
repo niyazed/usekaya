bc4ee4fdc316e20f9c1071bcbf04e99dc0f58f27
Niyaz Bin Hashem <niyaz.hashem@gmail.com>
2024-12-05 16:14:56 +0800

    feat: added read-replica for db operations

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,7 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    MASTER_DATABASE_URL: str
+    REPLICA_DATABASE_URL: str
     
     model_config = SettingsConfigDict(
         env_file=".env", 
--- src/db/database.py
+++ src/db/database.py
@@ -1,31 +1,30 @@
-
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import create_engine, SQLModel, Session
-from typing import Generator
+from fastapi import Request
 
+from src.utils.logger import logger
 from src.config.settings import settings
 from src.db.models import CampaignModel, AdGroupModel, AdGroupStatsModel
 
-'''
+
 engines = {
-    'master': create_engine('postgresql://user:***@localhost:5432/master',
+    'master': create_engine(settings.MASTER_DATABASE_URL,
                             logging_name='master'),
-    'replica': create_engine('postgresql://user:***@localhost:5432/replica',
+    'replica': create_engine(settings.REPLICA_DATABASE_URL,
                              logging_name='replica'),
 }
-'''
-engine = create_engine(url=settings.DATABASE_URL)
 
 async def init_db() -> None:
     """Creates all database tables defined in SQLModel models."""
-    SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engines['master'])
 
 
-def get_session() -> Generator[Session, None, None]:
-    """Creates a new session for the database."""
-    with Session(engine) as session:
+def get_session(request: Request):
+    # Determine engine based on context (e.g., request method)
+    engine = 'replica' if request.method == 'GET' else 'master'
+    logger.info(f"Using {engine} engine, method: {request.method}")
+    with Session(engines[engine]) as session:
         yield session
-    
 
 
 
